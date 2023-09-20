be62141977aa4349c108b1a16646e4e954911370
juan torres <juanonfleekmedia@gmail.com>
2023-09-20 18:46:50 -0500

    cambios

--- resources/views/layouts/whatsapp.blade.php
+++ resources/views/layouts/whatsapp.blade.php
@@ -21,45 +21,45 @@
                 
         <div class="wa__popup_content_list">
             				<div class="wa__popup_content_item ">
-				<a target="_blank" href="https://web.whatsapp.com/send?phone=51978209130&amp;text=texto" class="wa__stt wa__stt_online">
+				<a target="_blank" href="https://web.whatsapp.com/send?phone=51981497441&amp;text=texto" class="wa__stt wa__stt_online">
 				    				        <div class="wa__popup_avatar">
 				            <div class="wa__cs_img_wrap" style="background: url(https://demo.ninjateam.org/whatsapp-chat-wordpress/wp-content/uploads/sites/47/2018/11/17299bc407878ac0b9a914493b172fce_169x169.jpg) center center no-repeat; background-size: cover;"></div>
 				                </div>
 					        
                         <div class="wa__popup_txt">
-                            <div class="wa__member_name">Tommy Wilton</div>
+                            <div class="wa__member_name">Tattoo</div>
                             <!-- /.wa__member_name -->
-                            <div class="wa__member_duty">Customer Support</div>
+                            <div class="wa__member_duty">Descripcion del primer tipo de mensaje</div>
                             <!-- /.wa__member_duty -->
                                                     </div>
                         <!-- /.wa__popup_txt -->
                     </a>
                 </div>
             				<div class="wa__popup_content_item ">
-				<a target="_blank" href="https://web.whatsapp.com/send?phone=51978209130&amp;text=Hello, I have visited  and I need help from you. Here is link https://demo.ninjateam.org/whatsapp-chat-wordpress" class="wa__stt wa__stt_online">
+				<a target="_blank" href="https://web.whatsapp.com/send?phone=51981497441&amp;text=texto" class="wa__stt wa__stt_online">
 				    				        <div class="wa__popup_avatar">
 				            <div class="wa__cs_img_wrap" style="background: url(https://demo.ninjateam.org/whatsapp-chat-wordpress/wp-content/uploads/sites/47/2018/11/e979645000ac6cf7865e0b904df2817e_169x169.jpg) center center no-repeat; background-size: cover;"></div>
 				                </div>
 					        
                         <div class="wa__popup_txt">
-                            <div class="wa__member_name">Kelly Nguyen</div>
+                            <div class="wa__member_name">Pearcing</div>
                             <!-- /.wa__member_name -->
-                            <div class="wa__member_duty">Sale Support</div>
+                            <div class="wa__member_duty">Descripcion del segundo tipo de mensaje</div>
                             <!-- /.wa__member_duty -->
                                                     </div>
                         <!-- /.wa__popup_txt -->
                     </a>
                 </div>
             				<div class="wa__popup_content_item ">
-				<a target="_blank" href="https://web.whatsapp.com/send?phone=51978209130&amp;text=Hello, I have visit  and I need help from you. Here is link https://demo.ninjateam.org/whatsapp-chat-wordpress" class="wa__stt wa__stt_offline">
+				<a target="_blank" href="https://web.whatsapp.com/send?phone=51981497441&amp;text=texto" class="wa__stt wa__stt_offline">
 				    				        <div class="wa__popup_avatar">
 				            <div class="wa__cs_img_wrap" style="background: url(https://demo.ninjateam.org/whatsapp-chat-wordpress/wp-content/uploads/sites/47/2018/11/joye-avatar.png) center center no-repeat; background-size: cover;"></div>
 				                </div>
 					        
                         <div class="wa__popup_txt">
-                            <div class="wa__member_name">Joey Frank</div>
+                            <div class="wa__member_name">Tattoo</div>
                             <!-- /.wa__member_name -->
-                            <div class="wa__member_duty">Technical Support</div>
+                            <div class="wa__member_duty">Descripcion del tercer tipo de mensaje</div>
                             <!-- /.wa__member_duty -->
                                                         <div class="wa__member_status">
                                 I will be back soon                            </div>
