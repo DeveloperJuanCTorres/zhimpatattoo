9abef4a80c55441407630200ab4d7e0683472cf7
juan torres <juanonfleekmedia@gmail.com>
2023-09-21 15:29:00 -0500

    de

--- resources/views/artistaid.blade.php
+++ resources/views/artistaid.blade.php
@@ -49,7 +49,7 @@
                     @foreach($artes as $arte)
                     <div class="col-lg-3">
                         <div class="item">
-                            <img class="img-galeria" src="https://zhimpatattoosperu.com/storage/{{$arte}}" alt="">
+                            <img width="100%" class="img-galeria" src="https://zhimpatattoosperu.com/storage/{{$arte}}" alt="">
                         </div>
                     </div> 
                     @endforeach             
