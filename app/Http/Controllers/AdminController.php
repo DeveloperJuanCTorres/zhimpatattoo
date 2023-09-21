a3eeb55199baee19386a6d864a125e7c11576a48
juan torres <juanonfleekmedia@gmail.com>
2023-09-21 12:50:05 -0500

    cambios

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,8 +10,8 @@ class AdminController extends Controller
     public function home()
     {
         $artistas = Artist::all();
-        $artes[] = json_encode($artistas->artes);
-        dd($artes);
+        //$artes[] = json_encode($artistas->artes);
+        dd($artistas->artes);
         $nav = 'Inicio';
         return view('home',compact('nav','artistas'));
     }
