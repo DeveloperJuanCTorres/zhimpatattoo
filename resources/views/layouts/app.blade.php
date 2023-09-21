7ef64710958e50aa0fb1a1d167057645cae1cfff
juan torres <juanonfleekmedia@gmail.com>
2023-09-21 18:05:29 -0500

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Fonts -->
     <?php
-        $version = '1993.1.6';
+        $version = '1993.1.7';
     ?>
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
