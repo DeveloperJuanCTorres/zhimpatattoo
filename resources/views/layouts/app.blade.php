42ab6e84990201fa9eeeca5850a1844bf1101cb7
juan torres <juanonfleekmedia@gmail.com>
2023-09-25 16:39:16 -0500

    tipografia

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Fonts -->
     <?php
-        $version = '1993.2.8';
+        $version = '1993.2.9';
     ?>
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
